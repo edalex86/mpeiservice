4c238733643189a1c74f2c57ce519741b905f4a6
edalex86@gmail.com <edalex86@users.noreply.github.com>
2014-07-02 20:14:08 +0000

    Communication method changed to WCF over pipes Added Test Client Added Install, UnInstall, DownloadUpdates methods handling Added autoinstall feature Added logging class

---
+++ MPEIService/FileDownloader.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Threading;
+using MpeCore.Classes;
+using MpeCore;
+
+namespace MPEIService
+{
+    public class FileDownloader
+    {
+        private static string Source;
+        private static string Dest;
+        public static CompressionWebClient Client = new CompressionWebClient();
+
+        private void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                if (File.Exists(Dest))
+                {
+                    WaitForNoBusy();
+                    if (!Client.IsBusy)
+                    {
+                        try
+                        {
+                            File.Delete(Dest);
+                        }
+                        catch (Exception) { }
+                    }
+                }
+            }
+        }
+
+        private void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            if (e.TotalBytesToReceive > 0)
+            {
+            }
+            else
+            {
+            }
+        }
+
+
+        public FileDownloader(string source, string dest)
+        {
+            StartDownload(source, dest);
+        }
+
+        public static void StartDownload(string source, string dest)
+        {
+            Source = source;
+            Dest = dest;
+            //Client.DownloadProgressChanged += client_DownloadProgressChanged;
+            //Client.DownloadFileCompleted += client_DownloadFileCompleted;
+            Client.UseDefaultCredentials = true;
+            Client.Proxy.Credentials = CredentialCache.DefaultCredentials;
+            //Client.CachePolicy = new RequestCachePolicy();
+            Client.DownloadFile(source, dest);
+        }
+
+        private void DownloadFile_Shown(object sender, EventArgs e)
+        {
+            Client.DownloadFileAsync(new Uri(Source), Dest);
+        }
+
+
+        private void WaitForNoBusy()
+        {
+            int counter = 0;
+            while (Client.IsBusy || counter < 10)
+            {
+                counter++;
+                Thread.Sleep(100);
+            }
+        }
+
+        public static string GetPackageLocation(PackageClass packageClass, DownloadProgressChangedEventHandler downloadProgressChanged, AsyncCompletedEventHandler downloadFileCompleted)
+        {
+            string newPackageLocation = packageClass.GeneralInfo.Location;
+            if (!File.Exists(newPackageLocation))
+            {
+                newPackageLocation = packageClass.LocationFolder + packageClass.GeneralInfo.Id + ".mpe2";
+                if (!File.Exists(newPackageLocation))
+                {
+                    if (!string.IsNullOrEmpty(packageClass.GeneralInfo.OnlineLocation))
+                    {try
+                        {
+
+                            newPackageLocation = Path.GetTempFileName();
+                        StartDownload(packageClass.GeneralInfo.OnlineLocation, newPackageLocation);
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        finally
+                        {
+                        }
+                    }
+                }
+            }
+            return newPackageLocation;
+        }
+
+    }
+}
---
+++ MPEITestClient/MPEITestClient/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ServiceModel;
+using MpeCore.Classes;
+
+namespace MPEITestClient
+{
+    [ServiceContract]
+    public interface ICommand
+    {
+        [OperationContract]
+        bool Install(string ID, VersionInfo version);
+
+        [OperationContract]
+        bool UnInstall(string ID);
+
+        [OperationContract]
+        bool Update(string ID);
+
+        [OperationContract]
+        bool DownloadUpdates(bool all);
+
+        [OperationContract]
+        string Ping();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (NamedPipeClientStream client = new NamedPipeClientStream(".", "MPEITest", PipeDirection.InOut))
+            //{
+            //    Console.WriteLine("Connecting to server ...");
+            //    client.Connect(5000);
+            //    if(client.IsConnected)
+            //    Console.WriteLine("Connected.");
+
+            //    using (StreamWriter sw = new StreamWriter(client))
+            //    {
+
+            //        sw.WriteLine("В");
+            //        sw.Flush();
+            //    }
+            //    client.Close();
+            //}
+
+
+
+            ChannelFactory<ICommand> pipeFactory =
+              new ChannelFactory<ICommand>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress(
+                  "net.pipe://localhost/MPEIService"));
+            ICommand pipeProxy =
+              pipeFactory.CreateChannel();
+
+            while (true)
+            {
+                bool all = false;
+                Console.WriteLine(DateTime.Now + " action started");
+                //Console.WriteLine("pipe: " + pipeProxy.Install("b4293f64-9e83-4f1f-b2e3-8bdea2a37425", VersionInfo.Parse("1.1.5.0")));
+                Console.WriteLine("pipe: " + pipeProxy.UnInstall("b4293f64-9e83-4f1f-b2e3-8bdea2a37425"));
+                Console.WriteLine(DateTime.Now + " action finished");
+                Console.ReadKey();
+            }
+        }
+    }
+}
